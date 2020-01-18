43fc1ffea84aa25b5994cad5403f5b4c4188846c
Alexander Nosov <trashgenerator@gmail.com>
2020-01-18 17:47:16 +0100

    Updates

---
+++ precache-manifest.a44b5a55aca93047134d37b4eea30ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c38ee852e7e22cba3adbee1c0421e468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eeb6c5fbf8ade189a79",
+    "url": "/static/js/2.a289934c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a289934c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20feb56bd271f013f8cd",
+    "url": "/static/js/main.e93f74b2.chunk.js"
+  },
+  {
+    "revision": "1dea6e6f0a8b2395e37e",
+    "url": "/static/js/runtime-main.56a8ef0a.js"
+  }
+]);
